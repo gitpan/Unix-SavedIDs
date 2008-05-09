107e22db47b3d67522442428f8a98baf3607deb8
Dylan Djos Martin <dmartin@cpan.org>
2008-05-09 16:15:07 +0000

    Import of DMARTIN/Unix-SavedIDs-0.3.2 from CPAN.
    gitpan-cpan-distribution: Unix-SavedIDs
    gitpan-cpan-version:      0.3.2
    gitpan-cpan-path:         DMARTIN/Unix-SavedIDs-0.3.2.tar.gz
    gitpan-cpan-author:       DMARTIN
    gitpan-cpan-maturity:     released

--- lib/Unix/SavedIDs.pm
+++ lib/Unix/SavedIDs.pm
@@ -8,7 +8,7 @@ BEGIN {
 	our ($VERSION,@ISA,@EXPORT);
 	@ISA    = qw(Exporter);
 	@EXPORT = qw(getresuid getresgid setresuid setresgid);
-	$VERSION = '0.3.1';
+	use version ; $VERSION = qv('0.3.2');
 	use XSLoader;
 	# for some reason using $VERSION in XSLoader::load() blows up 
 	# on my OpenBSD box, so I'm coding it by hand.
